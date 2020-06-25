fa81844f80d03ed490303acb8df475ec42c7cc0e
Kate Kramer <katie@kramerdesign.ca>
2020-06-25 15:40:15 -0400

    initial commit

---
+++ src/Recipe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Recipe = ({ title, calories, ingredients, image }) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{calories}</p>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+      <img src={image} alt="" />
+    </div>
+  )
+}
+
+export default Recipe
